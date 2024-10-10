a2241ce98b8f6b6270e3ba4303158fa7dcf09fb9
Jose Mejia <josmejia.2401@gmail.com>
2024-10-10 12:54:18 -0500

    Actualizacion de pathname

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import * as React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, Link, useLocation } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 import CourseView from '../src/views/courses/view';
 
-function App() {
+function App(props) {
   const [preloader, setPreloader] = React.useState(true);
+  const [currentPathName, setCurrentPathName] = React.useState(null);
+
+  const handleClickOpenNav = (event) => {
+    if (event) {
+      event.preventDefault();
+      event.stopPropagation();
+      const navbarNavDropdown = document.getElementById("navbarNavDropdown");
+      navbarNavDropdown.classList.toggle("show");
+    }
+  }
   React.useEffect(() => {
+    setCurrentPathName(window.location.pathname);
     window.addEventListener('load', () => setPreloader(false));
-
-    const navbarTogglerIcon = document.getElementById("navbar-toggler-icon");
-    navbarTogglerIcon.addEventListener("click", function (event) {
-      if (event) {
-        event.preventDefault();
-        event.stopPropagation();
-        const navbarNavDropdown = document.getElementById("navbarNavDropdown");
-        console.log("TODO", navbarNavDropdown);
-        navbarNavDropdown.classList.toggle("show");
-      }
-
-    });
-
-
     /*const timeoutId = setTimeout(() => {
       setPreloader(false);
     }, 2000);*/
@@ -30,7 +27,7 @@ function App() {
       //clearTimeout(timeoutId);
       window.removeEventListener('load', () => setPreloader(false))
     }
-  }, [document]);
+  }, []);
   return (
     <Router>
       <div>
@@ -49,30 +46,44 @@ function App() {
 
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <Link className="navbar-brand" to="/">AppMa</Link>
+            <button className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              onClick={handleClickOpenNav}>
               <span id="navbar-toggler-icon" className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavDropdown">
               <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
                 <li className="nav-item">
-                  <Link className="nav-link active" aria-current="page" to="/auth/login">Home</Link>
+                  <Link className={`${currentPathName === '/auth/login' ? 'active' : ''} nav-link`} aria-current="page" to="/auth/login">Inicio</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" aria-current="page" to="/auth/login">Features</Link>
+                  <Link className={`${currentPathName === '/courses/view' ? 'active' : ''} nav-link`} aria-current="page" to="/courses/view">Cursos</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" aria-current="page" to="/auth/login">Pricing</Link>
+                  <Link className={`${currentPathName === '/about/view' ? 'active' : ''} nav-link`} aria-current="page" to="/about/view">Acerca de</Link>
                 </li>
                 <li className="nav-item dropdown">
-                  <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" to="/auth/login">Dropdown link</Link>
+                  <Link className={`${currentPathName === '/about/view' ? 'active' : ''} nav-link dropdown-toggle`} role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false" to="/blogs/view">Blogs</Link>
                   <ul className="dropdown-menu">
-                    <li><a className="dropdown-item" href="#">Action</a></li>
-                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                    <li>
+                      <Link className="dropdown-item" to="/auth/login">Action</Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/auth/login">Action2</Link>
+                    </li>
                   </ul>
                 </li>
 
+                <li className="nav-item">
+                  <Link className={`${currentPathName === '/contact/view' ? 'active' : ''} nav-link`} aria-current="page" to="/contact/view">Contáctanos</Link>
+                </li>
                 <li className="nav-item">
                   <button type="button" className="btn btn-lg button-primary">Iniciar sesión</button>
                 </li>
@@ -83,15 +94,20 @@ function App() {
             </div>
           </div>
         </nav>
-
-
-        <Routes>
-          <Route exact path="/auth/login" element={<CourseView></CourseView>} />
-          <Route path="*" element={<Navigate to={"/auth/login"} replace></Navigate>} />
-        </Routes>
+        <Inner />
       </div>
     </Router>
   );
+}
+
+
+function Inner() {
+  return (
+    <Routes location={useLocation()}>
+      <Route exact path="/auth/login" element={<CourseView></CourseView>} />
+      <Route path="*" element={<Navigate to={"/auth/login"} replace></Navigate>} />
+    </Routes>
+  )
 }
 
 export default App;
