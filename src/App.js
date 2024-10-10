6cdf4124eeb2ee7c4f29aedc5b68a8a4e9ddb3b1
Jose Mejia <josmejia.2401@gmail.com>
2024-10-10 11:05:40 -0500

    Actualizacion de navbar

--- src/App.js
+++ src/App.js
@@ -9,6 +9,20 @@ function App() {
   const [preloader, setPreloader] = React.useState(true);
   React.useEffect(() => {
     window.addEventListener('load', () => setPreloader(false));
+
+    const navbarTogglerIcon = document.getElementById("navbar-toggler-icon");
+    navbarTogglerIcon.addEventListener("click", function (event) {
+      if (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        const navbarNavDropdown = document.getElementById("navbarNavDropdown");
+        console.log("TODO", navbarNavDropdown);
+        navbarNavDropdown.classList.toggle("show");
+      }
+
+    });
+
+
     /*const timeoutId = setTimeout(() => {
       setPreloader(false);
     }, 2000);*/
@@ -16,7 +30,7 @@ function App() {
       //clearTimeout(timeoutId);
       window.removeEventListener('load', () => setPreloader(false))
     }
-  }, []);
+  }, [document]);
   return (
     <Router>
       <div>
@@ -32,30 +46,39 @@ function App() {
           </div>
         </div>
 
-        <nav className="navbar navbar-expand-lg shadow-sm dropstart">
+
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
             <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler dropdown-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span id="navbar-toggler-icon" className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse " id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
                 <li className="nav-item">
-                  <Link className="nav-link active li-nav-item" aria-current="page" to="/auth/login">Home</Link>
+                  <Link className="nav-link active" aria-current="page" to="/auth/login">Home</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link li-nav-item" aria-current="page" to="/auth/login#1">Link</Link>
+                  <Link className="nav-link" aria-current="page" to="/auth/login">Features</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link li-nav-item" aria-current="page" to="/auth/login#2">Categories</Link>
+                  <Link className="nav-link" aria-current="page" to="/auth/login">Pricing</Link>
                 </li>
-                <li className="nav-item">
-                  <Link className="nav-link li-nav-item disabled" aria-current="page" to="/auth/login#2" aria-disabled="true">Disabled</Link>
+                <li className="nav-item dropdown">
+                  <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" to="/auth/login">Dropdown link</Link>
+                  <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#">Action</a></li>
+                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
                 </li>
 
-                <li className="button-header margin-left "><a href="#" className="btn">Join</a></li>
-                <li className="button-header"><a href="login.html" className="btn btn3">Log in</a></li>
+                <li className="nav-item">
+                  <button type="button" className="btn btn-lg button-primary">Iniciar sesión</button>
+                </li>
+                <li className="nav-item">
+                  <button type="button" className="btn btn-lg button-secondary">Unirme</button>
+                </li>
               </ul>
             </div>
           </div>
