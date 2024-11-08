2f1c3ca92d9322bc6c97af65316b9e7941cab6a2
Jose Mejia <josmejia.2401@gmail.com>
2024-11-08 10:11:33 -0500

    Mejoras en navbar top

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate, Link, useLocation } from "react-router-dom";
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 import CourseView from '../src/views/courses/view';
@@ -31,6 +31,11 @@ function App(props) {
   return (
     <Router>
       <div>
+        {preloader && <div className="d-flex justify-content-center">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>}
 
         <nav className="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
           <div className="container px-5">
@@ -42,8 +47,10 @@ function App(props) {
             </button>
             <div className="collapse navbar-collapse" id="navbarResponsive">
               <ul className="navbar-nav ms-auto me-4 my-3 my-lg-0">
-                <li className="nav-item"><Link className="nav-link me-lg-3" to={"#features"}>Features</Link></li>
-                <li className="nav-item"><Link className="nav-link me-lg-3" to={"#download"}>Download</Link></li>
+                <li className="nav-item">
+                  <Link className={`${currentPathName === '/auth/login' ? 'active' : ''} nav-link me-lg-3`} to={"#features"}>Features</Link>
+                </li>
+                <Link className={`${currentPathName === '/auth/download' ? 'active' : ''} nav-link me-lg-3`} to={"#download"}>Download</Link>
               </ul>
               <button className="btn btn-primary rounded-pill px-3 mb-2 mb-lg-0" data-bs-toggle="modal" data-bs-target="#feedbackModal">
                 <span className="d-flex align-items-center">
@@ -55,7 +62,7 @@ function App(props) {
           </div>
         </nav>
 
-        <main>
+        <main style={{ maxWidth: '1328px' }}>
           <Inner {...props} />
         </main>
       </div>
