a14390fe63f5c0aff8f5a6d562995d788cac4293
Jose Mejia Martinez <josemejiamartinez@MacBook-Air-de-Jose.local>
2025-01-15 14:34:58 -0500

    Mejoras en App.js

--- src/App.js
+++ src/App.js
@@ -12,20 +12,52 @@ import ProjectsView from '../src/views/projects/view';
 import FunctionalitiesView from '../src/views/functionalities/view';
 import TasksView from '../src/views/tasks/view';
 import LogsView from '../src/views/record-logs/view';
-
 import ChatView from '../src/views/chats';
 
 
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      preloader: true,
+      currentPathName: "",
+      isAuthenticatedValue: false
+    };
+
+    this.refreshPage = this.refreshPage.bind(this);
+    this.validateStatus = this.validateStatus.bind(this);
+    this.checkDocumentState = this.checkDocumentState.bind(this);
+    this.handleClickOpenNav = this.handleClickOpenNav.bind(this);
+  }
+
+  componentDidMount() {
+    this.timeoutId = setInterval(() => this.validateStatus(), 1000);
+    this.checkDocumentState();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timeoutId);
+    document.removeEventListener('load', () => this.setState({ preloader: false }));
+  }
 
 
+  validateStatus = () => {
+    const auth = isAuthenticated();
+    const currentPathName = window.location.pathname;
+    const { isAuthenticatedValue } = this.state;
+    this.setState({ currentPathName: currentPathName || "" });
 
-function App(props) {
-  const [preloader, setPreloader] = React.useState(true);
-  const [currentPathName, setCurrentPathName] = React.useState(null);
-  const [isAuthenticatedValue, setIsAuthenticatedValue] = React.useState(false);
+    if (isAuthenticatedValue !== auth) {
+      this.setState({ isAuthenticatedValue: auth });
+    }
+    if (auth === false) {
+      this.refreshPage(currentPathName);
+    }
+  }
 
-  const refreshPage = () => {
-    const exists = [currentPathName || "", window.location.pathname]
+  refreshPage = (currentPathName) => {
+    const exists = [currentPathName, window.location.pathname]
       .filter(p => p.includes("auth"))[0];
     if (!exists) {
       storage.clear();
@@ -33,25 +65,16 @@ function App(props) {
     }
   }
 
-  const validateStatus = () => {
-    setCurrentPathName(window.location.pathname);
-    if (isAuthenticatedValue !== isAuthenticated()) {
-      setIsAuthenticatedValue(isAuthenticated());
-    }
-    if (isAuthenticated() === false) {
-      refreshPage();
-    }
-  }
 
-  const checkDocumentState = () => {
+  checkDocumentState = () => {
     if (document.readyState === "complete") {
-      setPreloader(false);
+      this.setState({ preloader: false });
     } else {
-      window.addEventListener('load', () => setPreloader(false));
+      window.addEventListener('load', () => this.setState({ preloader: false }));
     }
   }
 
-  const handleClickOpenNav = (event) => {
+  handleClickOpenNav = (event) => {
     if (event) {
       event.preventDefault();
       event.stopPropagation();
@@ -59,41 +82,34 @@ function App(props) {
       navbarNavDropdown.classList.toggle("show");
     }
   }
-  React.useEffect(() => {
-    const timeoutId = setInterval(() => validateStatus(), 1000);
-    return () => clearInterval(timeoutId);
-  }, []);
-  React.useEffect(() => {
-    checkDocumentState();
-    return () => document.removeEventListener('load', () => setPreloader(false));
-  }, []);
-  return (
-    <Router>
+
+  render() {
+    return (<Router>
       <div className="main-router">
-        {preloader && <div className="d-flex justify-content-center-spinner">
+        {this.state.preloader && <div className="d-flex justify-content-center-spinner">
           <div className="spinner-border" role="status">
             <span className="visually-hidden">Loading...</span>
           </div>
         </div>}
 
-        {isAuthenticatedValue && <nav className="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
+        {this.state.isAuthenticatedValue && <nav className="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
           <div className="container px-5">
             <Link className="navbar-brand fw-bold">AppMa</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"
-              onClick={handleClickOpenNav}>
+              onClick={this.handleClickOpenNav}>
               Menú
               <i className="fa-solid fa-bars ms-2"></i>
             </button>
             <div className="collapse navbar-collapse" id="navbarResponsive">
               <ul className="navbar-nav ms-auto me-4 my-3 my-lg-0">
                 <li className="nav-item">
-                  <Link className={`${currentPathName === '/projects' ? 'active' : ''} nav-link me-lg-3`} to={"/projects"}>Proyectos</Link>
+                  <Link className={`${this.state.currentPathName === '/projects' ? 'active' : ''} nav-link me-lg-3`} to={"/projects"}>Proyectos</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className={`${currentPathName === '/functionalities' ? 'active' : ''} nav-link me-lg-3`} to={"/functionalities"}>Funcionalidades</Link>
+                  <Link className={`${this.state.currentPathName === '/functionalities' ? 'active' : ''} nav-link me-lg-3`} to={"/functionalities"}>Funcionalidades</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className={`${currentPathName === '/tasks' ? 'active' : ''} nav-link me-lg-3`} to={"/tasks"}>Tareas</Link>
+                  <Link className={`${this.state.currentPathName === '/tasks' ? 'active' : ''} nav-link me-lg-3`} to={"/tasks"}>Tareas</Link>
                 </li>
               </ul>
               <button className="btn btn-primary rounded-pill px-3 mb-2 mb-lg-0" data-bs-toggle="modal" data-bs-target="#feedbackModal">
@@ -107,13 +123,12 @@ function App(props) {
         </nav>}
 
         <main>
-          <Inner {...props} />
+          <Inner {...this.props} isAuthenticatedValue={this.state.isAuthenticatedValue}></Inner>
         </main>
       </div>
-    </Router>
-  );
+    </Router>);
+  }
 }
-
 
 function Inner(props) {
   return (
@@ -125,10 +140,9 @@ function Inner(props) {
       <Route exact path="/functionalities" element={<FunctionalitiesView {...props} location={useLocation()} navigate={useNavigate()}></FunctionalitiesView>} />
       <Route exact path="/tasks" element={<TasksView {...props} location={useLocation()} navigate={useNavigate()}></TasksView>} />
       <Route exact path="/logs" element={<LogsView {...props} location={useLocation()} navigate={useNavigate()}></LogsView>} />
-
       <Route exact path="/chats" element={<ChatView {...props} location={useLocation()} navigate={useNavigate()}></ChatView>} />
 
-      <Route path="*" element={<Navigate to={"/auth/login"} replace></Navigate>} />
+      <Route path="*" element={<Navigate to={props.isAuthenticated ? "/projects" : "/auth/login"} replace></Navigate>} />
     </Routes>
   )
 }
